e75acfebf75008a5cc495884bfd1d6591634c012
Stu Behan <33100049+StuBehan@users.noreply.github.com>
2021-05-04 18:27:53 +0100

    added transaction and tests

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -14,8 +14,17 @@ describe('Account', () => {
     it('has an array called accountHistory', () =>{
       expect(account.accountHistory).toBeInstanceOf(Array);
     })
+
+    it('has a default balance of 0', () => {
+      expect(account.balance).toBe(0)
+    })
   })
 
-  
+  describe('Transaction', () => {
+    it('increases the balance of the account if credit', () => {
+      account.transaction(500, 'credit');
+      expect(account.balance).toBe(500);
+    })
+  })
 
 })
--- src/account.js
+++ src/account.js
@@ -1,6 +1,13 @@
 class Account {
   constructor(name) {
-    this.name = name;
+    this.name = name
     this.accountHistory = [];
+    this.balance = 0;
+  }
+
+  transaction = (amount, type) => {
+    if (type === 'credit') {
+      this.balance += amount
+    }
   }
 }
